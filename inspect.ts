265329c367085fd9ed8670895b83d7f25b515bfb
Zehua Chen <zehua-chen@outlook.com>
2026-03-26 21:24:05 -0700

    Create inspect script.

---
+++ inspect.ts
@@ -0,0 +1,14 @@
+import { parse } from '@plist/plist';
+
+const [, , ...args] = process.argv;
+const fileName = args[0];
+
+if (args.length !== 1 || !fileName) {
+  console.error('Usage: bun run inspect.ts <file>');
+  process.exit(1);
+}
+
+const input = await Bun.file(fileName).arrayBuffer();
+const result = parse(input);
+
+console.log(result);
