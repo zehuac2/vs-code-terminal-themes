d9bed633313bf8e5a3ef8cb69577ac09259b5d54
Zehua Chen <zehua-chen@outlook.com>
2026-03-26 23:20:25 -0700

    Create github action that build profiles on release.

--- index.ts
+++ index.ts
@@ -2,9 +2,14 @@ import { resolveTheme } from './theme';
 import { exportForIterm2 } from './iterm2';
 import { themes } from './themes';
 
+const GENERATED_DIR = 'generated';
+
+await Bun.$`mkdir -p ${GENERATED_DIR}`;
+
 for (const [themeName, definition] of Object.entries(themes)) {
   const theme = resolveTheme(themes, themeName);
   const fileName = `${definition.displayName ?? themeName}.itermcolors`;
+  const filePath = `${GENERATED_DIR}/${fileName}`;
 
-  await Bun.write(Bun.file(fileName), exportForIterm2(theme));
+  await Bun.write(Bun.file(filePath), exportForIterm2(theme));
 }
